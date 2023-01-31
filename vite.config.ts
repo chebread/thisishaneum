dca2529221d4436cf059ba8fec56343303579133
Cha Haneum <developer_haneum_11@icloud.com>
2023-02-01 01:35:09 +0900

    cleancode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths"; // 절대경로 맵핑 모듈
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths'; // vite의 절대경로 맵핑 모듈
 
 // https://vitejs.dev/config/
 export default defineConfig({
